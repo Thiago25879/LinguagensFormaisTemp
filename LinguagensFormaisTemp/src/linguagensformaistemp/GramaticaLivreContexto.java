510b1d8952375d6dd8557371ee86316ec5dd9f79
thiag <thiag@DESKTOP-D2BU948>
2020-09-20 21:15:41 -0300

    Classe principal criada

---
+++ LinguagensFormaisTemp/src/linguagensformaistemp/GramaticaLivreContexto.java
@@ -0,0 +1,5 @@
+package linguagensformaistemp;
+
+public class GramaticaLivreContexto {
+    
+}
--- LinguagensFormaisTemp/src/linguagensformaistemp/LinguagensFormaisTemp.java
+++ LinguagensFormaisTemp/src/linguagensformaistemp/LinguagensFormaisTemp.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package linguagensformaistemp;
 
-/**
- *
- * @author thiag
- */
 public class LinguagensFormaisTemp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Teste");
         int f = 1;
