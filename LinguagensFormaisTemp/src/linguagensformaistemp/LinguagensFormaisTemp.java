acb667978130728e6bd37fc3880a26a44af44816
thiag <thiag@DESKTOP-D2BU948>
2020-09-20 21:06:42 -0300

    Isso é apenas um teste de branch

--- LinguagensFormaisTemp/src/linguagensformaistemp/LinguagensFormaisTemp.java
+++ LinguagensFormaisTemp/src/linguagensformaistemp/LinguagensFormaisTemp.java
@@ -16,6 +16,7 @@ public class LinguagensFormaisTemp {
      */
     public static void main(String[] args) {
         System.out.println("Teste");
+        System.out.println("TestBranch");
     }
     
 }
