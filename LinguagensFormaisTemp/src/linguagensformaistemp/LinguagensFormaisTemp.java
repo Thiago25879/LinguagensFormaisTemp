52171f96b882a23644af1dc3f4c917b35314a7e8
thiag <thiag@DESKTOP-D2BU948>
2020-09-20 21:15:55 -0300

    Classe principal criada

--- LinguagensFormaisTemp/src/linguagensformaistemp/LinguagensFormaisTemp.java
+++ LinguagensFormaisTemp/src/linguagensformaistemp/LinguagensFormaisTemp.java
@@ -3,8 +3,7 @@ package linguagensformaistemp;
 public class LinguagensFormaisTemp {
 
     public static void main(String[] args) {
-        System.out.println("Teste");
-        int f = 1;
+        
     }
     
 }
